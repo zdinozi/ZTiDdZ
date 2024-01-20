e23c64169b75a0b121329bc5dea1c1eccadf770a
DESKTOP-QC2N73S\Zdi <zdi724@gmail.com>
2024-01-20 13:34:38 +0100

    fix too long password

--- Ćw 3/Cw3.2.py
+++ Ćw 3/Cw3.2.py
@@ -2,7 +2,7 @@ from random import randint, choice, shuffle
 import string
 
 def generate_safe_password():
-    letterList = ([string.punctuation, choice(string.digits), choice(string.ascii_lowercase), choice(string.ascii_uppercase)])
+    letterList = ([choice(string.punctuation), choice(string.digits), choice(string.ascii_lowercase), choice(string.ascii_uppercase)])
     for i in range(randint(4,15)):
         letterList.append(choice(string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits))
     shuffle(letterList)
