85396be0ae9f1164e9161c774677e93977de1769
DESKTOP-QC2N73S\Zdi <zdi724@gmail.com>
2024-01-20 13:29:13 +0100

    change variable name

--- Ćw 3/Cw3.2.py
+++ Ćw 3/Cw3.2.py
@@ -2,10 +2,10 @@ from random import randint, choice, shuffle
 import string
 
 def generate_safe_password():
-    passwordsList = ([string.punctuation, choice(string.digits), choice(string.ascii_lowercase), choice(string.ascii_uppercase)])
+    letterList = ([string.punctuation, choice(string.digits), choice(string.ascii_lowercase), choice(string.ascii_uppercase)])
     for i in range(randint(4,15)):
-        passwordsList.append(choice(string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits))
-    shuffle(passwordsList)
-    return ''.join(passwordsList)
+        letterList.append(choice(string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits))
+    shuffle(letterList)
+    return ''.join(letterList)
 
 print(generate_safe_password())
